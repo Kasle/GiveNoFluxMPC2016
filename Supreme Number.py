f442a0ee5c3f63a15df0a9467bc91fcd73405dc2
Kasle <aleks.mercik@outlook.com>
2016-03-12 12:29:34 -0500

    Fix

--- Supreme Number.py
+++ Supreme Number.py
@@ -2,8 +2,8 @@ start = raw_input()
 seperated = list(start)
 output = 0
 while len(seperated)>1:
-    print seperated
     for i in seperated:
         output+=int(i);
     seperated = list(str(output))
+    output = 0
 print seperated[0]
